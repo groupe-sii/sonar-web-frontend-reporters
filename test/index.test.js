67125bd1a3db455c4464c97d96163af366019a93
Valentin GOT <gotvalentin@gmail.com>
2016-11-30 16:16:24 +0100

    test(eslint): test ESLintReporter

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,7 @@
-const reporterFactory = require('./reporters/reporter.factory.test');
-const cssLintReporter = require('./reporters/csslint.reporter.test');
-
+const reporterFactory = require('./reporters/reporter.factory.test'),
+  cssLintReporter = require('./reporters/csslint.reporter.test'),
+  esLintReporter = require('./reporters/eslint.reporter.test');
 
 reporterFactory();
 cssLintReporter();
+esLintReporter();
---
+++ test/reporters/eslint.reporter.test.js
@@ -0,0 +1,97 @@
+require('chai').should();
+
+const fs = require('fs'),
+  ESLintReporter = require('../../index').ESLintReporter,
+  readJSONFile = require('../test.utils').readJSONFile,
+  esLintMock = require('./eslint.reporter.mock');
+
+module.exports = () => {
+
+  describe('ESLintReporter', () => {
+
+    describe('#launch', () => {
+
+      it('should throw an error if the project name is undefined', () => {
+        (() => new ESLintReporter(esLintMock.defaultOptions)).should.throw(Error);
+      });
+
+      it('should be the right project name', (done) => {
+        let reporter = new ESLintReporter(esLintMock.defaultOptions, 'SonarWebFrontEndReporters');
+
+        reporter.launch(() => {
+          readJSONFile(esLintMock.defaultOptions.report).project.should.be.equal('SonarWebFrontEndReporters');
+
+          done();
+        });
+      });
+
+      it('should create the output file', (done) => {
+        let reporter = new ESLintReporter(esLintMock.defaultOptions, 'SonarWebFrontEndReporters');
+
+        reporter.launch(() => {
+          fs.existsSync(esLintMock.defaultOptions.report);
+
+          done();
+        });
+      });
+
+      it('should be the right number of files', (done) => {
+        let reporter = new ESLintReporter(esLintMock.defaultOptions, 'SonarWebFrontEndReporters');
+
+        reporter.launch(() => {
+          let result = readJSONFile(esLintMock.defaultOptions.report);
+
+          result.files.length.should.be.equal(1);
+          result.nbFiles.should.be.equal(1);
+
+          done();
+        });
+      });
+
+      it ('should have an issue', (done) => {
+        let reporter = new ESLintReporter(esLintMock.defaultOptions, 'SonarWebFrontEndReporters');
+
+        reporter.launch(() => {
+          let result = readJSONFile(esLintMock.defaultOptions.report);
+
+          result.files[0].issues.length.should.be.equal(1);
+          result.files[0].issues[0].rulekey.should.be.equal('no-unused-vars');
+
+          done();
+        });
+      });
+
+      it ('should be a one line file', () => {
+        let reporter = new ESLintReporter(esLintMock.defaultOptions, 'SonarWebFrontEndReporters');
+
+        reporter.launch((done) => {
+          let result = readJSONFile(esLintMock.defaultOptions.report);
+
+          result.files[0].nbLines.should.be.equal(1);
+
+          done();
+        });
+      });
+
+      it ('shouldn\'t have processed files', () => {
+        let reporter = new ESLintReporter(esLintMock.badSrcOption, 'SonarWebFrontEndReporters');
+
+        reporter.launch((done) => {
+          let result = readJSONFile(esLintMock.badSrcOption.report);
+
+          result.files.length.should.be.equal(0);
+          result.nbFiles.should.be.equal(0);
+
+          done();
+        });
+      });
+
+      it('should find the rules file', () => {
+        (() => new ESLintReporter(esLintMock.badRulesFileOption, 'SonarWebFrontEndReporters')).should.throw(Error);
+      });
+
+    });
+
+  });
+
+};
