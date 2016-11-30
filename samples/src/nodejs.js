634df58f1379b70904b1e26631d1918aee36d3ce
Valentin GOT <gotvalentin@gmail.com>
2016-11-30 11:36:33 +0100

    feat(nodejs): handle NodeJS use

---
+++ samples/src/nodejs.js
@@ -0,0 +1,28 @@
+const SonarWebReporters = require('../../index').Reporters;
+
+let sonarWebReporters = new SonarWebReporters('Sonar Web Reporters', {
+  "csslint": false,
+  "eslint": {
+    "src": "samples/src/**/*.js",
+    "report": "reports/sonar/eslint.json",
+    "rulesFile": "./samples/.eslintrc"
+  },
+  "eslintangular": {
+    "src": "samples/src/**/*.js",
+    "report": "reports/sonar/eslintangular.json",
+    "rulesFile": "./samples/.eslintangularrc"
+  },
+  "jshint": {
+    "src": "samples/src/**/*.js",
+    "report": "reports/sonar/jshint.json",
+    "rulesFile": "./samples/.jshintrc"
+  },
+  "htmlhint": {
+    "src": "samples/src/**/*.html",
+    "report": "reports/sonar/htmlhint.json",
+    "rulesFile": "./samples/.htmlhintrc"
+  }
+});
+sonarWebReporters.launchReporters(() => {
+  console.log('All reporters have been processed');
+});
