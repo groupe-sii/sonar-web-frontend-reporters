830e51f73b3271e67619564b8634df9ab25ac37e
Olivier LE LAY <olelay@RNSPC153.sii.fr>
2016-12-01 09:44:30 +0100

    test(sass-lint): add sass tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,9 @@
 const reporterFactory = require('./reporters/reporter.factory.test'),
   cssLintReporter = require('./reporters/csslint.reporter.test'),
+  sassLintReporter = require('./reporters/sasslint.reporter.test'),
   esLintReporter = require('./reporters/eslint.reporter.test');
 
 reporterFactory();
 cssLintReporter();
+sassLintReporter();
 esLintReporter();
---
+++ test/reporters/sasslint.reporter.test.js
@@ -0,0 +1,81 @@
+require('chai').should();
+
+const SASSLintReporter = require('../../reporters/sasslint.reporter'),
+  readJSONFile = require('../test.utils').readJSONFile,
+  sassLintMock = require('./sasslint.reporter.mock'),
+  fs = require('fs');
+
+module.exports = () => {
+
+  describe('SASSLintReporter', () => {
+
+    describe('#launch', () => {
+
+      it('should throw an error if the project name is undefined', () => {
+        (() => new SASSLintReporter(sassLintMock.defaultOptions)).should.throw(Error);
+      });
+
+      it('should be the right project name', (done) => {
+        let reporter = new SASSLintReporter(sassLintMock.defaultOptions, 'test');
+        reporter.launch(() => {
+          readJSONFile(sassLintMock.defaultOptions.report).project.should.be.equal('test');
+          done();
+        });
+      });
+
+      it('should create the output file', (done) => {
+        let reporter = new SASSLintReporter(sassLintMock.defaultOptions, 'test');
+        reporter.launch(() => {
+          fs.existsSync(sassLintMock.defaultOptions.report).should.be.equal(true);
+          done();
+        });
+      });
+
+      it('should be the right file number', (done) => {
+        let reporter = new SASSLintReporter(sassLintMock.defaultOptions, 'test');
+        reporter.launch(() => {
+          let result = readJSONFile(sassLintMock.defaultOptions.report);
+          result.files.length.should.be.equal(1);
+          result.nbFiles.should.be.equal(1);
+          done();
+        });
+      });
+
+      it('should have foor issues', (done) => {
+        let reporter = new SASSLintReporter(sassLintMock.defaultOptions, 'test');
+        reporter.launch(() => {
+          let result = readJSONFile(sassLintMock.defaultOptions.report);
+          result.files[0].issues.length.should.be.equal(4);
+          done();
+        });
+      });
+
+      it('should have 4 lines', (done) => {
+        let reporter = new SASSLintReporter(sassLintMock.defaultOptions, 'test');
+        reporter.launch(() => {
+          let result = readJSONFile(sassLintMock.defaultOptions.report);
+          result.files[0].nbLines.should.be.equal(4);
+          done();
+        });
+      });
+
+      it('should not have processed files', (done) => {
+        let reporter = new SASSLintReporter(sassLintMock.badFileSrcOptions, 'test');
+        reporter.launch(() => {
+          let result = readJSONFile(sassLintMock.badFileSrcOptions.report);
+          result.files.length.should.be.equal(0);
+          result.nbFiles.should.be.equal(0);
+          done();
+        });
+      });
+
+      it('should throw bad file exception', () => {
+        (() => new SASSLintReporter(sassLintMock.badRulesFileOptions, 'test')).should.throw(Error);
+      });
+
+
+    });
+
+  });
+
+};
