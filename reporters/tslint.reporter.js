a8899dc68bd0c8a4c5f53c023169f7843112d1d2
Olivier LE LAY <olelay@RNSPC153.sii.fr>
2016-12-01 15:14:48 +0100

    chore(ts-lint): use object instead of string concat

--- reporters/tslint.reporter.js
+++ reporters/tslint.reporter.js
@@ -1,7 +1,6 @@
 const Reporter = require('./reporter'),
   TSLint = require('tslint'),
-  glob = require('glob'),
-  fs = require('fs');
+  glob = require('glob');
 
 module.exports = class TSLintReporter extends Reporter {
 
@@ -20,7 +19,7 @@ module.exports = class TSLintReporter extends Reporter {
 
   launch (done) {
     glob(this.options.src, (er, files) => {
-      this.processFiles(files, this.options);
+      this.processFiles(files);
       this.closeReporter(this.options.report);
 
       if (typeof done === 'function') {
@@ -29,46 +28,25 @@ module.exports = class TSLintReporter extends Reporter {
     });
   }
 
-  processFiles (fileArray, options) {
-    this.openReporter(options.report);
+  processFiles (fileArray) {
+    this.openReporter();
     fileArray.forEach((file) => {
-      this.processFile(file, options);
+      this.processFile(file);
     });
   }
 
-  processFile (file, options) {
+  processFile (file) {
     let input = this.readFile(file),
-      tslint = new TSLint.Linter({ formatter: 'prose' }, undefined),
-      severity,
-      d = (new Date()).getTime(),
-      index = 0;
+      tslint = new TSLint.Linter({ formatter: 'prose' }, undefined);
 
     let configuration = TSLint.Configuration.findConfiguration(this.options.rulesFile || null, file).results;
     tslint.lint(file, input, configuration);
 
-    let fileNbViolations = this.openFileIssues(file, options.report, null, /^(\s+)?\n$/gm);
+    this.openFileIssues(file, null, /^(\s+)?\n$/gm);
     let result = tslint.getResult();
     for (let message of result.failures) {
-      severity = 'INFO';
-      fileNbViolations[this.INFO]++;
-
-      fs.appendFileSync(options.report,
-        `{
-            "line": ${message.startPosition.lineAndCharacter.line + 1},
-            "col": ${message.startPosition.lineAndCharacter.character + 1},
-            "message": ${JSON.stringify(message.failure)},
-            "description": ${JSON.stringify(message.failure)},
-            "rulekey": "${message.ruleName}",
-            "severity": "${severity}",
-            "reporter": "tslint",
-            "creationDate": "${d}"
-          }` +
-        ((index < (result.failures.length) - 1) ? ',' : ''));
-
-      index++;
+      this.addIssue(message.startPosition.lineAndCharacter.line + 1, JSON.stringify(message.failure), JSON.stringify(message.failure), message.ruleName, this.INFO, 'tslint');
     }
-
-    this.closeFileIssues(fileNbViolations, options.report);
   }
 
 
