61bd6c62c6889f1c62939d5dc631d7ad1b2bb209
Valentin GOT <gotvalentin@gmail.com>
2016-12-01 10:07:13 +0100

    test(eslint-angular): test ESLintAngularReporter

--- test/index.test.js
+++ test/index.test.js
@@ -1,11 +1,13 @@
 const reporterFactory = require('./reporters/reporter.factory.test'),
   cssLintReporter = require('./reporters/csslint.reporter.test'),
   sassLintReporter = require('./reporters/sasslint.reporter.test'),
-  htmlHintReporter = require('./reporters/htmlhint.reporter.test'),
-  esLintReporter = require('./reporters/eslint.reporter.test');
+  esLintReporter = require('./reporters/eslint.reporter.test'),
+  esLintAngularReporter = require('./reporters/eslint-angular.reporter.test'),
+  htmlHintReporter = require('./reporters/htmlhint.reporter.test');
 
 reporterFactory();
 cssLintReporter();
 sassLintReporter();
 esLintReporter();
+esLintAngularReporter();
 htmlHintReporter();
