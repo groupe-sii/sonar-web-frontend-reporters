d2f2ee2ab5a461727a850848a3ab318366f094e3
Valentin GOT <gotvalentin@gmail.com>
2016-12-01 12:03:03 +0100

    test(jshint): test JSHintReporter

--- test/index.test.js
+++ test/index.test.js
@@ -2,14 +2,16 @@ const reporterFactory = require('./reporters/reporter.factory.test'),
   cssLintReporter = require('./reporters/csslint.reporter.test'),
   sassLintReporter = require('./reporters/sasslint.reporter.test'),
   htmlHintReporter = require('./reporters/htmlhint.reporter.test'),
-  tsLintReporter = require('./reporters/tslint.reporter.test'),
+  esLintReporter = require('./reporters/eslint.reporter.test'),
   esLintAngularReporter = require('./reporters/eslint-angular.reporter.test'),
-  esLintReporter = require('./reporters/eslint.reporter.test');
+  jsHintReporter = require('./reporters/jshint.reporter.test'),
+  tsLintReporter = require('./reporters/tslint.reporter.test');
 
 reporterFactory();
 cssLintReporter();
 sassLintReporter();
-esLintReporter();
-tsLintReporter();
-esLintAngularReporter();
 htmlHintReporter();
+esLintReporter();
+esLintAngularReporter();
+jsHintReporter();
+tsLintReporter();
