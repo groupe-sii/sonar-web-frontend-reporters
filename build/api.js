32b73d2c527ce6ba90d8a25d232410282031f09e
Valentin GOT <gotvalentin@gmail.com>
2016-12-07 11:30:47 +0100

    feat(es5): add ES5 sub-object to require ES5 reporters

--- build/api.js
+++ build/api.js
@@ -1,12 +1,22 @@
 'use strict';
 
 module.exports = {
-  Reporters: require('./reporters/all.reporter'),
+  Reporters: require('./reporters'),
   CSSLintReporter: require('./reporters/csslint.reporter'),
   ESLintReporter: require('./reporters/eslint.reporter'),
   ESLintAngularReporter: require('./reporters/eslint-angular.reporter'),
   HTMLHintReporter: require('./reporters/htmlhint.reporter'),
   JSHintReporter: require('./reporters/jshint.reporter'),
   TSLintReporter: require('./reporters/tslint.reporter'),
-  SASSLintReporter: require('./reporters/sasslint.reporter')
+  SASSLintReporter: require('./reporters/sasslint.reporter'),
+  ES5: {
+    Reporters: require('../build/reporters'),
+    CSSLintReporter: require('../build/reporters/csslint.reporter'),
+    ESLintReporter: require('../build/reporters/eslint.reporter'),
+    ESLintAngularReporter: require('../build/reporters/eslint-angular.reporter'),
+    HTMLHintReporter: require('../build/reporters/htmlhint.reporter'),
+    JSHintReporter: require('../build/reporters/jshint.reporter'),
+    TSLintReporter: require('../build/reporters/tslint.reporter'),
+    SASSLintReporter: require('../build/reporters/sasslint.reporter')
+  }
 };
