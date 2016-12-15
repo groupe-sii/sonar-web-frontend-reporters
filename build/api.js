c20a1ef8a1a98e83eb1f6ad9eb5de01c6ed685de
Valentin GOT <gotvalentin@gmail.com>
2016-12-15 19:43:50 +0100

    fix(es5): re-build ES5 files

--- build/api.js
+++ build/api.js
@@ -9,16 +9,5 @@ module.exports = {
   JSHintReporter: require('./reporters/jshint.reporter'),
   TSLintReporter: require('./reporters/tslint.reporter'),
   SASSLintReporter: require('./reporters/sasslint.reporter'),
-  SASSToSCSSLintReporter: require('./reporters/sass-to-scsslint.reporter'),
-  ES5: {
-    Reporters: require('../build/reporters'),
-    CSSLintReporter: require('../build/reporters/csslint.reporter'),
-    ESLintReporter: require('../build/reporters/eslint.reporter'),
-    ESLintAngularReporter: require('../build/reporters/eslint-angular.reporter'),
-    HTMLHintReporter: require('../build/reporters/htmlhint.reporter'),
-    JSHintReporter: require('../build/reporters/jshint.reporter'),
-    TSLintReporter: require('../build/reporters/tslint.reporter'),
-    SASSLintReporter: require('../build/reporters/sasslint.reporter'),
-    SASSToSCSSLintReporter: require('../build/reporters/sass-to-scsslint.reporter')
-  }
+  SASSToSCSSLintReporter: require('./reporters/sass-to-scsslint.reporter')
 };
