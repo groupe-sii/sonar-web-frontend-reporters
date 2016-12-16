7046d62f00d0852e318f260faf6fcf6d5c5254cb
Valentin GOT <gotvalentin@gmail.com>
2016-12-16 17:16:34 +0100

    test(jshint): tests for multi glob patterns

--- test/reporters/jshint.reporter.test.js
+++ test/reporters/jshint.reporter.test.js
@@ -77,6 +77,19 @@ module.exports = () => {
         });
       });
 
+      it ('shouldn\'t match the ignored file', (done) => {
+        let reporter = new JSHintReporter(jsHintMock.multiSrcOption, 'SonarWebFrontEndReporters');
+
+        reporter.launch(() => {
+          let result = readJSONFile(jsHintMock.multiSrcOption.report);
+
+          result.files.length.should.be.equal(1);
+          result.nbFiles.should.be.equal(1);
+
+          done();
+        });
+      });
+
       it ('shouldn\'t have processed files', () => {
         let reporter = new JSHintReporter(jsHintMock.badSrcOption, 'SonarWebFrontEndReporters');
 
